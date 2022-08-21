4187412ad8444f29aa3a13540157e5971c8cae78
Jerry Yang <efJerryYang@outlook.com>
2022-08-21 14:54:43 +0800

    feat: grade `primes` pass (but qemu fail)

--- user/primes.c
+++ user/primes.c
@@ -3,7 +3,7 @@
 
 int sieve_primes(int *primes, int n) // maybe can be replaced by while loop
 {
-    if (n == 1)
+    if (n == 0)
         return 0;
 
     int p[2];
@@ -27,13 +27,13 @@ int sieve_primes(int *primes, int n) // maybe can be replaced by while loop
         }
         close(p[0]);
 
-        fprintf(1, "prime %d\n", primes[0]);
-        // fprintf(1, "prime %d \tpid=%d\t", primes[0], getpid());
+        // fprintf(1, "prime %d\n", primes[0]);
+        fprintf(1, "prime %d \tpid=%d\n", primes[0], getpid());
         // fprintf(1, "numbers:");
         // for (int k = 0; k <= i; k++)
         //     fprintf(1, " %d", primes[k]);
         // fprintf(1, "\n");
-        sieve_primes(primes + 1, i + 1);
+        sieve_primes(primes + 1, i);
 
         // write(p2[1], primes, 4 * (i + 1));
         write(p2[1], "e", 1);
@@ -52,12 +52,24 @@ int sieve_primes(int *primes, int n) // maybe can be replaced by while loop
         char ch;
         read(p2[0], &ch, 1);
         close(p2[0]);
+        // fprintf(1, "\t\t\t\tmain proc pid=%d\n", getpid());
         return 0;
     }
 }
 int main()
 {
     // no argument passed
+
+    /**
+     * @brief 我的写法有一点问题，函数调用发生在了子进程中，
+     * 如果不返回函数调用（直接exit(0)，会导致整个子进程终止）
+     *
+     * 现在我已经能运行产生正确的素数序列了，只是终止存在问题，
+     * 我要考虑的是如何告诉主进程调用完成的消息。
+     *
+     * 我现在是在子进程中完成的打印
+     *
+     */
     // int numbers[34];
     int primes[34];
     int p[2];
@@ -83,13 +95,13 @@ int main()
                 --i;
             j++;
         }
-        fprintf(1, "prime %d\n", primes[0]);
-        // fprintf(1, "prime %d \tpid=%d\t", primes[0], getpid());
+        // fprintf(1, "prime %d\n", primes[0]);
+        fprintf(1, "prime %d \tpid=%d\n", primes[0], getpid());
         // fprintf(1, "numbers:");
         // for (int k = 0; k <= i; k++)
         //     fprintf(1, " %d", primes[k]);
         // fprintf(1, "\n");
-        sieve_primes(primes + 1, i + 1);
+        sieve_primes(primes + 1, i);
         // it seems that can only remain 1 function call here
         // write(p2[1], primes, 4 * (i + 1));
         write(p2[1], "e", 1);
