8029406c710daaa1d8ef961894b31c95cad620ca
Jerry Yang <efJerryYang@outlook.com>
2022-12-17 00:49:50 +0800

    chore: barrier deadlock

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,6 +30,30 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
+  // printf("barrier: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+  pthread_mutex_lock(&bstate.barrier_mutex);
+
+  bstate.nthread++;
+  
+  if(bstate.nthread == nthread){ 
+    printf("barrier: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    while (bstate.nthread != 0) {
+      printf("sleep: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+      printf("wakeup: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+    }
+  }
+  
+  // bstate.nthread = 0;
+  // bstate.round++;
+
+  // pthread_cond_broadcast(&bstate.barrier_cond);
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
   
 }
 
