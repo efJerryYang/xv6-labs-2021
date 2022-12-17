eada081b11a3f215722a9833dc7ea218a497cfe4
Jerry Yang <efJerryYang@outlook.com>
2022-12-17 11:31:04 +0800

    fix: deadlock when one thread race around the loop
    The bug is caused by the thread who broadcasts other threads racing in the next round to reach the barrier while loop before the last thread exits the while loop, which will cause the last thread be stuck in the cond_wait.
    
    I fixed this by adding a local variable to record the round number when enter the loop, if it is changed, that means another round should have begun, and the barrier condition has already met. So we can just skip the while loop if the round changed.

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,31 +30,35 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  // printf("barrier: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+  // printf("==================== Entering barrier() ====================\n");
   pthread_mutex_lock(&bstate.barrier_mutex);
-
+  printf("nthread = %d, round = %d\n", bstate.nthread, bstate.round);
   bstate.nthread++;
-  
-  if(bstate.nthread == nthread){ 
-    printf("barrier: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
-    bstate.nthread = 0;
-    bstate.round++;
-    pthread_cond_broadcast(&bstate.barrier_cond);
-  } else {
-    while (bstate.nthread != 0) {
-      printf("sleep: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
-      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
-      printf("wakeup: nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+  while(bstate.nthread != nthread && bstate.nthread != 0){
+    int current_round = bstate.round;
+    // printf("Entering cond_wait:\n");
+    // printf("  nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+
+    // printf("Exiting cond_wait:\n");
+    // printf("  nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+    if (current_round != bstate.round) {
+      // printf("  round changed!\n");
+      break;
     }
   }
-  
-  // bstate.nthread = 0;
-  // bstate.round++;
-
-  // pthread_cond_broadcast(&bstate.barrier_cond);
-
+  // printf("Exited while loop:\n");
+  // printf("  nthread = %d, round = %d\n", bstate.nthread, bstate.round);
+  if (bstate.nthread == nthread) {
+    // printf("Broadcasting...\n");
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    bstate.nthread = 0;
+    bstate.round++;
+    // printf("Broadcasted!\n");
+  }
+  // printf("==================== Exiting barrier() =====================\n");
   pthread_mutex_unlock(&bstate.barrier_mutex);
-  
 }
 
 static void *
@@ -100,3 +104,48 @@ main(int argc, char *argv[])
   }
   printf("OK; passed\n");
 }
+/**
+ * Analysis of a deadlock in the barrier() function.
+ * 
+ * The deadlock occurs when the last thread to reach the barrier is the last thread to exit the while loop.
+ *
+ * Terminal output:
+    ==================== Entering barrier() ====================  // Thread 0 enters barrier
+    nthread = 0, round = 0                                        // Thread 0 acquires lock
+    Entering cond_wait:                                           // Thread 0 enters cond_wait
+      nthread = 1, round = 0                                      // Thread 0 releases lock and waits
+    ==================== Entering barrier() ====================  // Thread 1 enters barrier
+    nthread = 1, round = 0                                        // Thread 1 acquires lock
+    Exited while loop:                                            // Thread 1 skips cond_wait
+      nthread = 2, round = 0                                      
+    Broadcasting...                                               // Thread 1 broadcasts
+    Broadcasted!
+    ==================== Exiting barrier() =====================  // Thread 1 exits barrier, releases lock
+    Exiting cond_wait:                                            // Thread 0 wakes up, acquires lock
+      nthread = 0, round = 1
+    Exited while loop:                                            // Thread 0 satisfies while loop condition and exits
+      nthread = 0, round = 1
+    ==================== Exiting barrier() =====================  // Thread 0 exits barrier, releases lock
+    ==================== Entering barrier() ====================  // Thread 0 enters barrier
+    nthread = 0, round = 1                                        // Thread 0 acquires lock
+    Entering cond_wait:                                           // Thread 0 enters cond_wait
+      nthread = 1, round = 1                                      // Thread 0 releases lock and waits
+    ==================== Entering barrier() ====================  // Thread 1 enters barrier
+    nthread = 1, round = 1                                        // Thread 1 acquires lock
+    Exited while loop:                                            // Thread 1 skips cond_wait
+      nthread = 2, round = 1
+    Broadcasting...                                               // Thread 1 broadcasts
+    Broadcasted!
+    ==================== Exiting barrier() =====================  // Thread 1 exits barrier, releases lock
+    ==================== Entering barrier() ====================  // Thread 1 enters barrier
+    nthread = 0, round = 2                                        // Thread 1 acquires lock
+    Entering cond_wait:                                           // Thread 1 enters cond_wait
+      nthread = 1, round = 2
+    Exiting cond_wait:                                            // Bugs here, if a thread broadcasted other threads and then raced in the next round 
+      nthread = 1, round = 2                                      // and reached the barrier before the last thread exited the while loop, the last thread
+    Entering cond_wait:                                           // would be stuck in the cond_wait.
+      nthread = 1, round = 2
+ * 
+ * 
+ * 
+ */
